d0e4e896ebbf03b3e948a9692099045d9bba5663
Ashish Mali <abmali81292@gmail.com>
2026-03-03 13:20:04 +0530

    Output_Parser - demo 01

--- test.py
+++ test.py
@@ -1,8 +1,23 @@
-from dotenv import load_dotenv
-import huggingface_hub, os
+# from dotenv import load_dotenv
+# import huggingface_hub, os
 
-load_dotenv()  # THIS IS REQUIRED
+# load_dotenv()  # THIS IS REQUIRED
 
-print("hf hub:", huggingface_hub.__version__)
-print("token loaded:", bool(os.getenv("HUGGINGFACEHUB_API_TOKEN")))
+# print("hf hub:", huggingface_hub.__version__)
+# print("token loaded:", bool(os.getenv("HUGGINGFACEHUB_API_TOKEN")))
+
+# Group the Employees by Department
+emp = {
+    "Rahul" : "HR",
+    "Amit": "IT",
+    "Neha": "IT",
+    "Sita": "HR"
+}
+
+dict_ = {}
+
+for e, d in emp.items():
+    dict_.setdefault(d, []).append(e)
+
+print(dict_)
 
